31cb363088378a7f115e9d93610fe3bbb151536f
João <joaopaulobastos@proton.me>
2026-04-30 21:29:59 -0400

    Adicionado segunda parte do ex03

--- arrays-ponteiros/aula-07-08/ex03.c
+++ arrays-ponteiros/aula-07-08/ex03.c
@@ -13,5 +13,11 @@ int main() {
         printf("v[%d]: %d\n", i, *(p + i));
     }
 
+    char* pc;
+    for (int i = 0; i < N; i++) {
+        pc = (char*) (p + i);
+        printf("Primeiro byte de v[%d]: %d\n", i, *pc & 0xFF);
+    }
+
     return 0;
 }
